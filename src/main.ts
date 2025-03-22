d59b0863f295f7ce0d97ded5ef3831f095b6500e
Rick Groenevelt <rickgroenevelt2000@gmail.com>
2025-03-22 18:07:18 +0100

    Restyled current components with Tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import '@fontsource-variable/open-sans/wdth.css'
+import '@fontsource-variable/open-sans/wdth-italic.css'
 
-createApp(App).mount('#app')
+createApp(App).mount('body')
